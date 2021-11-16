d4e20194d8561c8c90361b99aa00fa020c860943
oscar <meet@oscarfdez.com>
2021-11-16 16:40:15 +0100

    images slides

--- src/app/api/cities.js
+++ src/app/api/cities.js
@@ -2,49 +2,49 @@ export default [
     {
     id: 1,
     name: "Barcelona",
-    image: "http://lorempixel.com/400/200/city",
+    image: "https://images.unsplash.com/photo-1617122287896-8638ac5c5414?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80",
     excerpt:
     "Scenikus es una plataforma internacional dedicada.",
     },
     {
     id: 2,
     name: "Navarra",
-    image: "http://lorempixel.com/400/200/city",
+    image: "https://images.unsplash.com/photo-1617122287896-8638ac5c5414?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80",
     excerpt:
     "Scenikus es una plataforma internacional dedicada.",
     },
     {
     id: 3,
     name: "Valencia",
-    image: "http://lorempixel.com/400/200/city",
+    image: "https://images.unsplash.com/photo-1617122287896-8638ac5c5414?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80",
     excerpt:
     "Scenikus es una plataforma internacional dedicada.",
     },
     {
     id: 4,
     name: "Madrid",
-    image: "http://lorempixel.com/400/200/city",
+    image: "https://images.unsplash.com/photo-1617122287896-8638ac5c5414?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80",
     excerpt:
     "Scenikus es una plataforma internacional dedicada.",
     },
     {
     id: 5,
     name: "Granada",
-    image: "http://lorempixel.com/400/200/city",
+    image: "https://images.unsplash.com/photo-1617122287896-8638ac5c5414?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80",
     excerpt:
     "Scenikus es una plataforma internacional dedicada.",
     },
     {
     id: 1,
     name: "Bilbao",
-    image: "http://lorempixel.com/400/200/city",
+    image: "https://images.unsplash.com/photo-1617122287896-8638ac5c5414?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80",
     excerpt:
     "Scenikus es una plataforma internacional dedicada.",
     },
     {
     id: 6,
     name: "Vigo",
-    image: "http://lorempixel.com/400/200/city",
+    image: "https://images.unsplash.com/photo-1617122287896-8638ac5c5414?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80",
     excerpt:
     "Scenikus es una plataforma internacional dedicada.",
     },
